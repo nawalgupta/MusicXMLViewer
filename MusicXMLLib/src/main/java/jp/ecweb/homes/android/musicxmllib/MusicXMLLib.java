f1dd20e2db8f599bf1ede2e3d43ec618a5acd0ac
Takashi Kakinuma <kakinuma.takashi@homes.ecweb.jp>
2018-10-01 07:57:00 +0900

    新規作成

--- MusicXMLLib/src/main/java/jp/ecweb/homes/android/musicxmllib/MusicXMLLib.java
+++ MusicXMLLib/src/main/java/jp/ecweb/homes/android/musicxmllib/MusicXMLLib.java
@@ -1,4 +1,19 @@
 package jp.ecweb.homes.android.musicxmllib;
 
+import org.w3c.dom.Document;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public class MusicXMLLib {
+
+	private Document mDocument;
+
+	public MusicXMLLib(File xmlFile) throws Exception {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		mDocument = builder.parse(xmlFile);
+	}
 }
--- app/src/main/java/jp/ecweb/homes/android/musicxmlviewer/MainActivity.java
+++ app/src/main/java/jp/ecweb/homes/android/musicxmlviewer/MainActivity.java
@@ -3,11 +3,19 @@ package jp.ecweb.homes.android.musicxmlviewer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import jp.ecweb.homes.android.musicxmllib.MusicXMLLib;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		try {
+			MusicXMLLib musicXMLLib = new MusicXMLLib(null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
